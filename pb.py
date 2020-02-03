8ffb13e36655cf2ee45053a4947b955c65e55b30
luke stonier <lukestonier@hotmail.com>
2020-02-03 17:07:55 +0000

    initial commit

---
+++ pb.py
@@ -0,0 +1,112 @@
+import cv2
+import datetime
+import time
+import copy
+
+webcam = None
+key = None
+frame = None
+
+def __draw_label(img, text, pos, bg_color):
+    font_face = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 0.4
+    color = (255, 255, 255)
+    thickness = cv2.FILLED
+    margin = 2
+
+    txt_size = cv2.getTextSize(text, font_face, scale, thickness)
+
+    end_x = pos[0] + txt_size[0][0] + margin
+    end_y = pos[1] - txt_size[0][1] - margin
+
+    cv2.rectangle(img, pos, (end_x, end_y), bg_color, thickness)
+    cv2.putText(img, text, pos, font_face, scale, color, 1, cv2.LINE_AA)
+
+def setup():
+    print("setup")
+    global webcam
+    global key
+    key = cv2.waitKey(1)
+    webcam = cv2.VideoCapture(0)
+    print("Ready")
+    startAppLoop()
+
+def startAppLoop():
+    global key
+    global frame
+    while True:
+        check, frame = webcam.read()
+        cv2.imshow("Capturing", frame)
+        try:
+            key = cv2.waitKey(1)
+
+            if key == ord("s"):
+                takePicture()
+                #setup()
+            elif key == ord("q"):
+                endProcess()
+                break
+
+        except (KeyboardInterrupt):
+            endProcess()
+            break
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
+
+def takePicture():
+    global frame
+    global webcam
+    print("Take Picture")
+    date = datetime.datetime.now()
+    fileName = date.strftime("%d-%m-%Y-%H-%M-%S")
+    imageFileName = "./Data/" + fileName + ".jpg"
+    cv2.imwrite(filename= imageFileName, img=frame)
+    print("Image saved!")
+    takeVideo(fileName)
+
+def takeVideo(fileName):
+    global frame
+    global webcam
+    print("Take Video")
+    videoFileName = "./Data/" + fileName + ".avi"
+
+    #start
+
+    width = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+    height = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+    size = (width, height)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(videoFileName, fourcc, 20.0, size)
+    videoLength = 5
+
+    now = time.time()
+    end = now + videoLength
+
+    while(time.time() < end):
+        _, _frame = webcam.read()
+        textFrame = copy.copy(_frame)
+        __draw_label(textFrame, 'RECORDING', (25,25), (0,0,0))
+        cv2.imshow('Capturing', textFrame)
+        out.write(_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    #end
+    print("Video saved!")
+
+def endProcess():
+    global webcam
+    print("Turning off camera.")
+    webcam.release()
+    print("Camera off.")
+    print("Program ended.")
+    cv2.destroyAllWindows()
+
+def postData():
+    print("Make request")
+
+setup()
