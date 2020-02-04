2e2c73de8448c219e4e4101599b90c1f88a4712c
luke stonier <lukestonier@hotmail.com>
2020-02-04 20:51:12 +0000

    new contants

--- pb.py
+++ pb.py
@@ -76,8 +76,8 @@ def takeVideo(fileName):
 
     #start
 
-    width = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
-    height = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+    width = int(webcam.get(cv2.CV_CAP_PROP_FRAME_WIDTH) + 0.5)
+    height = int(webcam.get(cv2.CV_CAP_PROP_FRAME_HEIGHT) + 0.5)
     size = (width, height)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(videoFileName, fourcc, 20.0, size)
